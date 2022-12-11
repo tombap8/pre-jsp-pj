8da4a4a094647d261a1a3e404124d283475f3039
tombraceparker <tombap8@hotmail.com>
2022-12-11 16:03:18 +0900

    ss

--- src/common/Paging.java
+++ src/common/Paging.java
@@ -1,49 +1,48 @@
 package common;
 
 public class Paging {
-	
+
 	// 페이징 DTO 생성
 	PagingDTO pgdto = new PagingDTO();
 	// DB연결 클래스 생성하기
 	JDBConnector jdbc = new JDBConnector();
-	// 파라미터 공유
-	public static String numBk,colPm,keyPm;
+	// 파라미터 공유변수(다른메서드에서도 사용)
+	public static String colPm, keyPm, numBk;
+	// colPm - 파라미터 pmCol을 담는다
+	// keyPm - 파라미터 pmKey를 담는다
+	// numBk - 페이징블록순번
 
 	///////////////////////
 	// 생성자 메서드 /////////
 	///////////////////////
 	// 역할: 인스턴스 생성시 바로 실행하므로 기본 변수값을 모두 셋팅한다!
-	public Paging(String tbName,String bkNum, String pmCol,String pmKey) { 
-		// tbName - 페이징대상테이블
+	public Paging(String tbName, String bkNum, String pmCol, String pmKey) {
+		// tbName - 페이징대상테이블 / bkNum - 페이징블록순번 / pmCol - 검색항목 / pmKey - 검색어
 
-		numBk = bkNum;
+		// pmCol과 pmKey 전달변수를 전역변수에 할당!
 		colPm = pmCol;
 		keyPm = pmKey;
-		/********************************* 
-		15. 페이징 링크 생성하기
-		______________________________
+		// 전달된 페이징블록순번도 전역변수에 할당!
+		numBk = bkNum;
 
-		1)시작 레코드번호 : startNum
-		2)페이지당 레코드개수 : onePageCnt
-		3)전체 레코드수 : totalCnt
-		4)리스트 그룹수 : listGroup
-			(전체개수 ÷ 페이지당개수) 
-		5)남은 레코드수 : etcRecord
-		6)페이징링크 코드 저장변수 pgCode
-
+		/*********************************
+		 * 15. 페이징 링크 생성하기 ______________________________
+		 * 
+		 * 1)시작 레코드번호 : startNum 2)페이지당 레코드개수 : onePageCnt 3)전체 레코드수 : totalCnt 4)리스트
+		 * 그룹수 : listGroup (전체개수 ÷ 페이지당개수) 5)남은 레코드수 : etcRecord 6)페이징링크 코드 저장변수 pgCode
+		 * 
 		 *********************************/
 		try {
 			// 15-1. 전체 레코드 수 구하기
 			// 레코드수 구하기 쿼리
-			String cntQuery = "SELECT COUNT(*) FROM "+ tbName;
-			
-			// 1.5. 만약 검색어가 있으면 쿼리 변경!
-			if(pmKey!=null) {
-				cntQuery += " WHERE `"+pmCol+"` "
-						+ "LIKE \"%"+pmKey+"%\"";
+			String cntQuery = "SELECT COUNT(*) FROM " + tbName;
+
+			// 만약 검색어가 있으면 쿼리 추가하기!
+			if (pmKey != null) {
+				cntQuery += " WHERE `" + pmCol + "` " + "LIKE \"%" + pmKey + "%\" ";
 				System.out.println("널이 아냐!");
 			}
-			
+
 			// 쿼리를 PreparedStatement에 넣기
 			jdbc.pstmt = jdbc.conn.prepareStatement(cntQuery);
 			// 쿼리실행! -> 개수정보를 리턴받아 ResultSet에 담는다!
@@ -56,31 +55,22 @@ public class Paging {
 			} ////// if ///////////
 
 			// 15-2. 리스트 그룹수 : 전체개수 ÷ 페이지당개수
-			pgdto.setListGroup(
-					pgdto.getTotalCnt() / pgdto.getOnePageCnt());
+			pgdto.setListGroup(pgdto.getTotalCnt() / pgdto.getOnePageCnt());
 
 			// 15-3. 남은 레코드수 : 전체개수 % 페이지당개수
 			// 나머지 구할땐 %연산자
-			pgdto.setEtcRecord(
-					pgdto.getTotalCnt() % pgdto.getOnePageCnt());
+			pgdto.setEtcRecord(pgdto.getTotalCnt() % pgdto.getOnePageCnt());
 
 			// 한계수 체크: 나머지가 있고 없고에 따라 1개차이남
-			pgdto.setLimit(
-			pgdto.getEtcRecord() == 0 ? 
-			pgdto.getListGroup() : pgdto.getListGroup() + 1);
+			pgdto.setLimit(pgdto.getEtcRecord() == 0 ? pgdto.getListGroup() : pgdto.getListGroup() + 1);
 			// 나머지가 있으면 1페이지 더 추가!
-			
-			
+
 			// ####### 페이징 블록 셋팅하기 ########
 			// 8.페이징 단위개수(oneBlockCnt)
 			// 9.페이징 그룹수 : (리스트그룹수+남은레코드수) ÷ 페이징 단위개수 (blockGroup)
-			pgdto.setBlockGroup(
-			(pgdto.getListGroup()+pgdto.getEtcRecord())/pgdto.getOneBlockCnt());
+			pgdto.setBlockGroup((pgdto.getListGroup() + pgdto.getEtcRecord()) / pgdto.getOneBlockCnt());
 			// 10.남은 페이징수 : (리스트그룹수+남은레코드수) % 페이징 단위개수 (etcBlock)
-			pgdto.setEtcBlock(
-			(pgdto.getListGroup()+pgdto.getEtcRecord())%pgdto.getOneBlockCnt());
-			
-
+			pgdto.setEtcBlock((pgdto.getListGroup() + pgdto.getEtcRecord()) % pgdto.getOneBlockCnt());
 
 		} /// try ////
 		catch (Exception e) {
@@ -98,7 +88,6 @@ public class Paging {
 
 	} /////// 생성자 메서드 ///////
 
-	
 	// 사용자 정의 메서드 1
 	//////////////////////////////////
 	/// 시작번호 구해서 리턴하는 메서드 /////
@@ -107,13 +96,12 @@ public class Paging {
 	public int changeStartNum(String pgnum) {
 		// pgnum 변수는 리스트 페이지의 파라미터 pgnum을 가져옴!
 		// url/.../list.html?pgnum=3 이부분임!
-		/**************************************** 
-		[ 페이징 변수처리전 페이지번호로 시작번호 변경하기 ]
+		/****************************************
+		 * [ 페이징 변수처리전 페이지번호로 시작번호 변경하기 ]
 		 *****************************************/
 		// 페이지번호 가져오기
 		String pgNum = pgnum;
 		System.out.println("파라미터:" + pgNum);
-
 
 		// 파라미터가 있으면 시작값 처리하기
 		if (pgNum != null) { // null이 아니면!
@@ -142,39 +130,51 @@ public class Paging {
 	// 역할: 페이징 정보에 따라 실제 링크코드를 만들어서 본 html페이지로 리턴
 	public String makePaging() {
 		// 페이징링크 코드 저장변수
-		String pgCode="";
-		
+		String pgCode = "";
+
 		// #### 블록 시작값 ####
 		int bkStart = (Integer.parseInt(numBk) - 1) * pgdto.getOneBlockCnt();
 		// #### 블록 한계값 ####
 		int bkLimit = Integer.parseInt(numBk) * pgdto.getOneBlockCnt();
 		// #### 블록 한계값이 리스트그룹수 보다크면 리스트 그룹수로 정한다!
 		// (남은 레코드가 있으면 1더함)
-		if(bkLimit > pgdto.getListGroup()) 
-			bkLimit = pgdto.getListGroup()+(pgdto.getEtcRecord()>0?1:0);
+		if (bkLimit > pgdto.getListGroup())
+			bkLimit = pgdto.getListGroup() + (pgdto.getEtcRecord() > 0 ? 1 : 0);
+
+		// ######## 처음블록가기 ##########
+		if (Integer.parseInt(numBk) - 1 > 0) {
+			pgCode += "<a href='list.jsp?pgnum=1";
+			pgCode += "&bknum=1";
+			if (keyPm != null) {
+				pgCode += "&col=" + colPm + "&key=" + keyPm;
+			}
+			pgCode += "'>«</a> ";
+		} else {
+			pgCode += "<span style='opacity:.5'>«</span> ";
+		}
 		
 		// ######## 이전블록가기 ##########
-		if(Integer.parseInt(numBk)-1 > 0) {
-			pgCode += "<a href='list.jsp?pgnum=" 
-			+ (((Integer.parseInt(numBk)-1)*pgdto.getOneBlockCnt())
-					-(pgdto.getOneBlockCnt()-1));
-			pgCode += "&bknum=" +  + (Integer.parseInt(numBk)-1); 
-			if(keyPm!=null) {
-				pgCode += "&col="+colPm+"&key="+keyPm;
+		if (Integer.parseInt(numBk) - 1 > 0) {
+			pgCode += "<a href='list.jsp?pgnum="
+					+ (((Integer.parseInt(numBk) - 1) * pgdto.getOneBlockCnt()) - (pgdto.getOneBlockCnt() - 1));
+			pgCode += "&bknum=" +(Integer.parseInt(numBk) - 1);
+			if (keyPm != null) {
+				pgCode += "&col=" + colPm + "&key=" + keyPm;
 			}
 			pgCode += "'>◀</a> ";
-		}
-		else {
+		} else {
 			pgCode += "◁ ";
 		}
-		
-		System.out.println("현재블록번호:"+numBk);
-		System.out.println("이전블록:"+(Integer.parseInt(numBk)-1));
+
+		System.out.println("현재블록번호:" + numBk);
+		System.out.println("이전블록:" + (Integer.parseInt(numBk) - 1));
 
 		// ########################
 
 		// 15-4. 페이징 링크 코드 만들기
 //		for (int i = 0; i < pgdto.getLimit(); i++) {
+		// 모든페이징을 그리지말고 현재 블록만큼만 for문을 돌아야하므로
+		// bkStart 계산변수를 시작값으로하고 bkLimit 계산값을 한계값으로 넣어준다!
 		for (int i = bkStart; i < bkLimit; i++) {
 			// 만약 현재 페이지와 같은 번호는 a링크 걸지말고
 			// b태그로 두꺼운 글자 표시만 해주자!
@@ -183,26 +183,35 @@ public class Paging {
 			} /// if ////
 			else {
 				// pgCode변수에 모두 넣는다
-				pgCode += "<a href='list.jsp?pgnum=" + (i + 1); 
-				pgCode += "&bknum=" + numBk; 
-				if(keyPm!=null) {
-					pgCode += "&col="+colPm+"&key="+keyPm;
-				}
+				pgCode += "<a href='list.jsp?pgnum=" + (i + 1);
+				pgCode += "&bknum=" + numBk; // 페이징블록만 추가!
+				// 검색어가 있으면 검색어 파라미터를 만들어준다!!!
+				if (keyPm != null) {
+					pgCode += "&col=" + colPm + "&key=" + keyPm;
+				} /// if /////////////////////////////
 				pgCode += "'>" + (i + 1) + "</a>";
 			} /// else //////
 
-			// 사이바 찍기 
+			// 사이바 찍기
 			// (한계값-1,즉 마지막번호 전까지만 사이바출력)
+			// -> 페이징 블록이 적용된 경우 bkLimit 마지막 값을 적용한다!
 			if (i < bkLimit - 1) {
 				pgCode += " | ";
 			}
 
 		} ////////// for //////////////
 		
+
+		
+		// 마지막 블록번호 : 블록그룹수 + 나머지 블록수가 있음면 1더함
+		int lastBlockNum = pgdto.getBlockGroup()+(pgdto.getEtcBlock()>0?1:0);
+		System.out.println("마지막블록번호:"+lastBlockNum);
+
+		// 마지막 페이지번호 : 마지막 블록 전번호 * 한 페이지당 블록수 + 1
+		int lastPgNum = (lastBlockNum-1)*pgdto.getOneBlockCnt()+1;
+		
 		// ##### 다음블록가기 #######
-		if(Integer.parseInt(numBk)+1 < 
-				pgdto.getListGroup()/pgdto.getOneBlockCnt()
-				+pgdto.getEtcBlock()) {
+		if(Integer.parseInt(numBk)+1 <= lastBlockNum) {
 			pgCode += "<a href='list.jsp?pgnum=" 
 		+ ((Integer.parseInt(numBk)*pgdto.getOneBlockCnt())+1); 
 			pgCode += "&bknum=" + (Integer.parseInt(numBk)+1); 
@@ -216,16 +225,26 @@ public class Paging {
 		}
 		// ######################
 		
+		// ##### 마지막블록가기 #######
+		if(Integer.parseInt(numBk)+1 <= lastBlockNum) {
+			pgCode += "<a href='list.jsp?pgnum=" + lastPgNum; 
+			pgCode += "&bknum=" + lastBlockNum; //맨마지막 블록으로 이동!
+			if(keyPm!=null) {
+				pgCode += "&col="+colPm+"&key="+keyPm;
+			}
+			pgCode += "'>»</a>";
+		}
+		else {
+			pgCode += " <span style='opacity:.5'>»</span>";
+		}
+		// ######################
+		
 		System.out.println("다음블록:"+(pgdto.getBlockGroup()+pgdto.getEtcBlock()));
 		System.out.println("계산:"+(Integer.parseInt(numBk)+1));
-		
+
 		// 최종 결과 리턴하기!!!
 		return pgCode;
-		
+
 	} ////////// makePaging 메서드 //////////
-
-
-
-
 
 } ///////// 클래스 //////////
--- src/common/PagingDTO.java
+++ src/common/PagingDTO.java
@@ -4,23 +4,23 @@ package common;
 // DTO - Data Transfer Object 의 줄임말로
 // 데이터 캡슐화의 겟터,셋터가 있는 클래스를 부르는말임!
 public class PagingDTO {
-	// ***** 페이징 변수 ****** 
+	// ***** 페이징 변수 ******
 	// 1.시작 레코드번호 : LIMIT의 시작값
 	private int startNum;
 	// 2.페이지당 레코드개수 : LIMIT의 개수
-	final private int onePageCnt = 10;
+	final private int onePageCnt = 9;
 	// 3.전체 레코드수
 	private int totalCnt;
 	// 4.리스트 그룹수 : 전체개수 ÷ 페이지당개수
 	private int listGroup;
 	// 5.남은 레코드수 : 리스트 그룹에서 남은 레코드수
-	private int etcRecord;	
+	private int etcRecord;
 	// 6.파라미터 형변환 변수(현재 페이지번호)
 	private int pageSeq; // 기본값 1(파라미터가 없으면 1들어감!)
 	// 7.한계수 체크: 나머지가 있고 없고에 따라 1개차이남
 	private int limit;
 	// 8.페이징 단위개수
-	final private int oneBlockCnt = 10;
+	final private int oneBlockCnt = 11;
 	// 9.페이징 그룹수 : 리스트그룹수 ÷ 페이징 단위개수
 	private int blockGroup;
 	// 10.남은 페이징수 : 리스트그룹수 % 페이징 단위개수
@@ -31,64 +31,81 @@ public class PagingDTO {
 	public int getStartNum() {
 		return startNum;
 	}
+
 	public void setStartNum(int startNum) {
 		this.startNum = startNum;
 	}
+
 	// 2.페이지당 레코드개수 : LIMIT의 개수
 	// -> final 이므로 셋터가 없다!
 	public int getOnePageCnt() {
 		return onePageCnt;
 	}
+
 	// 3.전체 레코드수
 	public int getTotalCnt() {
 		return totalCnt;
 	}
+
 	public void setTotalCnt(int totalCnt) {
 		this.totalCnt = totalCnt;
 	}
+
 	// 4.리스트 그룹수 : 전체개수 ÷ 페이지당개수
 	public int getListGroup() {
 		return listGroup;
 	}
+
 	public void setListGroup(int listGroup) {
 		this.listGroup = listGroup;
 	}
+
 	// 5.남은 레코드수 : 리스트 그룹에서 남은 레코드수
 	public int getEtcRecord() {
 		return etcRecord;
 	}
+
 	public void setEtcRecord(int etcRecord) {
 		this.etcRecord = etcRecord;
 	}
+
 	// 6.파라미터 형변환 변수(현재 페이지번호)
 	public int getPageSeq() {
 		return pageSeq;
 	}
+
 	public void setPageSeq(int pageSeq) {
 		this.pageSeq = pageSeq;
 	}
+
 	// 7.한계수 체크: 나머지가 있고 없고에 따라 1개차이남
 	public int getLimit() {
 		return limit;
 	}
+
 	public void setLimit(int limit) {
 		this.limit = limit;
 	}
+
 	// 8.페이징 단위개수
 	public int getOneBlockCnt() {
 		return oneBlockCnt;
 	}
+
 	// 9.페이징 그룹수 : (리스트그룹수+남은레코드수) ÷ 페이징 단위개수
 	public int getBlockGroup() {
 		return blockGroup;
 	}
+
 	public void setBlockGroup(int blockGroup) {
 		this.blockGroup = blockGroup;
 	}
+
 	// 10.남은 페이징수 : (리스트그룹수+남은레코드수) % 페이징 단위개수
 	public int getEtcBlock() {
 		return etcBlock;
 	}
+
 	public void setEtcBlock(int etcBlock) {
 		this.etcBlock = etcBlock;
 	}
